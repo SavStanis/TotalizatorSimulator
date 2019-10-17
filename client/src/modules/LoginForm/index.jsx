e1dc2531f41af68e77223935898f0194cc608ba1
SavStanis <svsodessa@gmail.com>
2019-10-18 00:41:06 +0300

    Login and Registration pages added

---
+++ client/src/modules/LoginForm/index.jsx
@@ -0,0 +1,74 @@
+import React, {Component} from 'react';
+import {Link, Route} from 'react-router-dom';
+
+import "./LoginForm.css"
+import {Block, Button} from "../../components";
+
+class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        let login = "";
+        let loginValid = this.validateLogin(login);
+        let password = "";
+        let passwordValid = this.validatePassword(password);
+
+        this.onLoginChange = this.onLoginChange.bind(this);
+        this.onPasswordChange = this.onPasswordChange.bind(this);
+
+        this.state = {
+            login: login,
+            password: password,
+            loginValid: loginValid,
+            passwordValid: passwordValid,
+        };
+    }
+
+    validateLogin(login) {
+        return login.length >= 4;
+    }
+    validatePassword(password) {
+        return password.length >=8;
+    }
+
+    onLoginChange(e) {
+        let val = e.target.value;
+        let valid = this.validateLogin(val);
+        this.setState({login: val, loginValid: valid});
+    }
+    onPasswordChange(e) {
+        let val = e.target.value;
+        let valid = this.validatePassword(val);
+        this.setState({password: val, passwordValid: valid});
+    }
+
+    render() {
+        let loginColor = this.state.loginValid ? "green" : "red";
+        let passwordColor = this.state.passwordValid ? "green" : "red";
+
+        return (
+            <form action="" className="authForm">
+                <div className="header">
+                    <div className="headerText">Totalizator Simulator</div>
+                </div>
+                <div className="authBlock">
+                    <div className="authContent">
+                        <h1>Войдите в аккаунт</h1>
+                        <Block>
+                            <input type="text" className="authFormElem" placeholder="Login" value={this.state.login}
+                                   onChange={this.onLoginChange} style={{borderColor: loginColor}}/>
+                            <p></p>
+                            <input type="password" className="authFormElem" placeholder="Password" value={this.state.password}
+                                   onChange={this.onPasswordChange} style={{borderColor: passwordColor}}/>
+                            <div className="submitButton">
+                                <Button>Войти</Button>
+                            </div>
+                            <Link className="regLink" to="/register">Зарегистрироваться</Link>
+                        </Block>
+                    </div>
+                </div>
+            </form>
+        );
+    }
+}
+
+export default LoginForm;
--- client/src/pages/Auth/index.jsx
+++ client/src/pages/Auth/index.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import "./Auth.css";
-import { Block, Button,AuthForm } from "../../components";
+import {LoginForm, RegisterForm} from "../../modules";
 import { Link, Route } from "react-router-dom";
 
 const Auth = () => (
     <div className="auth">
-        <div className="header">
-            <div className="headerText">Totalizator Simulator</div>
-        </div>
-        <div className="authBlock">
-            <div className="authContent">
-                <h1>Войдите в аккаунт</h1>
-                <Block>
-                    <AuthForm login="" pswd=""/>
-                    <Link className="regLink" to="/register">Зарегистрироваться</Link>
-                </Block>
-            </div>
-        </div>
+        <Route exact path={["/","/login"]} component={LoginForm}/>
+        <Route exact path={"/register"} component={RegisterForm}/>
     </div>
 );
 
